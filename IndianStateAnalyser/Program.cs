7ba35ca34cb7b90c53475924d3f3b9a3c5a3423a
Akash <akashmane12598@gmail.com>
2020-10-27 18:36:41 +0530

    [Akash] Add. Master Code

---
+++ IndianStateAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Analyser Program!");
+        }
+    }
+}
